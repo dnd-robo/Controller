b10b1fe6b86a81e76351c97d8c9e974ce8afd356
Eric Olvera <Eric.Olvera@quisitive.com>
2020-08-02 18:50:10 -0500

    Updates

---
+++ precache-manifest.814444f9d801052236c163f7f07a2bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034d0ecc0aaa473ceb403db433b47d14",
+    "url": "/controller/index.html"
+  },
+  {
+    "revision": "d60e5085c5b6b0f66769",
+    "url": "/controller/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "305af6a1dbad035cfcee",
+    "url": "/controller/static/js/2.39706c24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/controller/static/js/2.39706c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60e5085c5b6b0f66769",
+    "url": "/controller/static/js/main.2567856f.chunk.js"
+  },
+  {
+    "revision": "de8229315687b30be803",
+    "url": "/controller/static/js/runtime-main.881d49b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/controller/static/media/logo.5d5d9eef.svg"
+  }
+]);
